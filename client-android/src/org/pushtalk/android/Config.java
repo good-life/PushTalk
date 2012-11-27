4a72f93d0576f90001cf1c38521fa75fe870fa83
good-life <pushtalk.org@gmail.com>
2012-11-27 09:56:48 +0800

    r0.2.1 version.

--- client-android/src/org/pushtalk/android/Config.java
+++ client-android/src/org/pushtalk/android/Config.java
@@ -8,7 +8,7 @@ public class Config {
 
     public static boolean IS_TEST_MODE = true;
     
-    public static String VERSION = "0.2.0";
+    public static String VERSION = "0.2.1";
     
     public static String WEB_JS_MODULE = "pushtalk";
     
--- server-java/src/org/pushtalk/server/Config.java
+++ server-java/src/org/pushtalk/server/Config.java
@@ -5,7 +5,7 @@ public class Config {
     // Should be assign value when db init
     public static String SERVER_ID;
     
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
     
     public static final int CACHE_DURATION_DAYS = 7;
     public static final int MESSAGE_CACHE_MAX_NUMBER = 100;
